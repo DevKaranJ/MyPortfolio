0f45d83d5cbcae531607a9f6cf81ea5202873f48
Karan Joshi <Karan190213@gmail.com>
2023-06-25 14:42:09 +0530

    files update

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-    document.addEventListener('DOMContentLoaded', function() {
-        var hamburger = document.querySelector('.hammenu');
-        var menuContent = document.querySelector('#menuContent');
-      
-        hamburger.addEventListener('click', function() {
-          menuContent.classList.toggle('show');
-        });
-      });
-      
-      document.addEventListener("DOMContentLoaded", function() {
-        const menuContent = document.getElementById("menuContent");
-        const closeButton = document.querySelector(".close-button");
-      
-        closeButton.addEventListener("click", function() {
-          menuContent.classList.remove("show");
-        });
-      });
-      
-      
+const hamburger = document.querySelector('.hammenu');
+const menuContent = document.querySelector('#menuContent');
+
+hamburger.addEventListener('click', () => {
+  menuContent.classList.toggle('show');
+});
+
+const closeButton = document.querySelector('.close-button');
+
+closeButton.addEventListener('click', () => {
+  menuContent.classList.remove('show');
+});
+    
