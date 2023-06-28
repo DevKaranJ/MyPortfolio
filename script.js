eaa6565a7cdd9802e472ba42262b47a53c8fb9fe
Karan Joshi <Karan190213@gmail.com>
2023-06-28 18:58:11 +0530

    Linters fixed

--- script.js
+++ script.js
@@ -23,19 +23,19 @@ menuItems.forEach((menuItem) => {
 
 // Form Validation
 
-const form = document.getElementById('contactForm');
-const emailInput = document.querySelector(`input[name='email']`);
-const errorEmail = document.getElementById('errorEmail');
+const form = document.getElementById("contactForm");
+const emailInput = document.querySelector("input[name='email']");
+const errorEmail = document.getElementById("errorEmail");
 
-form.addEventListener('submit', (event) => {
+form.addEventListener("submit", function(event) {
   event.preventDefault();
 
   const emailValue = emailInput.value;
 
-  if (emailValue !== emailValue.toLowerCase()) {
-    errorEmail.textContent = 'Email must be in lowercase.';
+  if (emailValue.toLowerCase() !== emailValue) {
+    errorEmail.textContent = "Email must be in lowercase.";
   } else {
-    errorEmail.textContent = '';
+    errorEmail.textContent = "";
     form.submit();
   }
 });
@@ -47,7 +47,7 @@ const contactForm = document.getElementById('contactForm');
 function saveFormData() {
   const formData = {
     name: contactForm.querySelector("[name='name']").value,
-    email: contactForm.querySelector(`input[name='email']`).value,
+    email: contactForm.querySelector("[name='email']").value,
     message: contactForm.querySelector("[name='message']").value,
   };
 
@@ -59,7 +59,7 @@ function loadFormData() {
 
   if (formData) {
     contactForm.querySelector("[name='name']").value = formData.name;
-    contactForm.querySelector(`input[name='email']`).value = formData.email;
+    contactForm.querySelector("[name='email']").value = formData.email;
     contactForm.querySelector("[name='message']").value = formData.message;
   }
 }
