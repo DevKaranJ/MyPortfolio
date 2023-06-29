6d128e80733c9263367582a04a7a62fdff960ca7
Karan Joshi <Karan190213@gmail.com>
2023-06-29 23:45:13 +0530

    html&js updated with project cards

---
+++ propop.js
@@ -0,0 +1,59 @@
+// Project popup menu //
+const projectSection = document.getElementById('projectcontainer');
+
+const project = [
+    {
+        id: 1,
+        image: 'resources/Snapshoot Portfolio.svg',
+        title: 'Multi-Post Stories Gain+Glory',
+        technologies: ['Ruby on Rails', 'CSS', 'HTML', 'JavaScript'],
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.",
+        liveLink: 'https://devkaranj.github.io/MyPortfolio/',
+        sourceCode: 'https://github.com/DevKaranJ/MyPortfolio',
+      },
+  {
+    id: 2,
+    image: 'resources/Snapshoot Portfolio.svg',
+    title: 'Multi-Post Stories Gain+Glory',
+    technologies: ['Ruby on Rails', 'CSS', 'HTML', 'JavaScript'],
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.",
+    liveLink: 'https://devkaranj.github.io/MyPortfolio/',
+    sourceCode: 'https://github.com/DevKaranJ/MyPortfolio',
+  },
+  {
+    id: 3,
+    image: 'resources/Snapshoot Portfolio.svg',
+    title: 'Multi-Post Stories Gain+Glory',
+    technologies: ['Ruby on Rails', 'CSS', 'HTML', 'JavaScript'],
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.",
+    liveLink: 'https://devkaranj.github.io/MyPortfolio/',
+    sourceCode: 'https://github.com/DevKaranJ/MyPortfolio',
+  },
+  {
+    id: 4,
+    image: 'resources/Snapshoot Portfolio.svg',
+    title: 'Multi-Post Stories Gain+Glory',
+    technologies: ['Ruby on Rails', 'CSS', 'HTML', 'JavaScript'],
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.",
+    liveLink: 'https://devkaranj.github.io/MyPortfolio/',
+    sourceCode: 'https://github.com/DevKaranJ/MyPortfolio',
+  },
+  {
+    id: 5,
+    image: 'resources/Snapshoot Portfolio.svg',
+    title: 'Multi-Post Stories Gain+Glory',
+    technologies: ['Ruby on Rails', 'CSS', 'HTML', 'JavaScript'],
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.",
+    liveLink: 'https://devkaranj.github.io/MyPortfolio/',
+    sourceCode: 'https://github.com/DevKaranJ/MyPortfolio',
+  },
+  {
+    id: 6,
+    image: 'resources/Snapshoot Portfolio.svg',
+    title: 'Multi-Post Stories Gain+Glory',
+    technologies: ['Ruby on Rails', 'CSS', 'HTML', 'JavaScript'],
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.",
+    liveLink: 'https://devkaranj.github.io/MyPortfolio/',
+    sourceCode: 'https://github.com/DevKaranJ/MyPortfolio',
+}];
+
