32fe4182155b7ab14ac32fe02195efd2f0215e47
Young-Gaius <youngehu8@gmail.com>
2023-06-29 08:55:57 +0100

    add js code

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ function saveFormData() {
   localStorage.setItem('contactFormData', JSON.stringify(formData));
 }
 
-
+contactForm.addEventListener('input', saveFormData);
 
 function loadFormData() {
   const formData = JSON.parse(localStorage.getItem('contactFormData'));
